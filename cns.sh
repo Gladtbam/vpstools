5fba8ece1e6016daeda86e7713f116a243de7366
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2024-07-30 16:15:39 +0800

    Update cns.sh

--- cns.sh
+++ cns.sh
@@ -11,9 +11,12 @@ if [ $EUID -ne 0 ]; then
     exit 1
 fi
 
-AddSite() {
+if [ -n $2 ]; then
     domain=$2
+    echo $domain
+fi
 
+AddSite() {
     if [ -f /etc/nginx/sites-available/$domain ]; then
         echo -e "${RED}域名已存在${NC}"
         exit 1
@@ -36,6 +39,14 @@ server {
     root /var/www/$domain;
     index index.html index.htm index.php;
 
+    location / {
+        return 301 https://\$host\$request_uri;
+    }
+
+    location ~ /.well-known {
+        allow all;
+    }
+
     access_log /var/log/nginx/$domain.access.log;
 }" > /etc/nginx/sites-available/$domain
 
@@ -55,7 +66,21 @@ server {
 }
 
 AddSSL() {
-    "/usr/local/bin/acme.sh"/acme.sh --issue -d $domain --webroot /var/www/$domain -w /var/www/html
+    "/usr/local/bin/acme.sh"/acme.sh --issue -d $domain --webroot /var/www/$domain -w /var/www/html --log
+    if [ $? -eq 0 ]; then
+        echo -e "${RED}证书申请失败${NC}"
+        rm /etc/nginx/sites-enabled/$domain
+        rm /etc/nginx/sites-available/$domain
+        rm -rf /var/www/$domain
+        exit 1
+    else
+        mkdir -p /etc/nginx/ssl/${domain}
+        "/usr/local/bin/acme.sh"/acme.sh --installcert -d $domain \
+            --fullchain-file /etc/nginx/ssl/${domain}/fullchain.cer \
+            --keypath /etc/nginx/ssl/${domain}/${domain}.key \
+            --ca-file /etc/nginx/ssl/${domain}/ca.cer \
+            --reloadcmd "systemctl reload nginx"
+    fi
 
     if [ $? -eq 0 ]; then
         echo " \
@@ -66,45 +91,47 @@ server {
     root /var/www/$domain;
     index index.html index.htm index.php;
 
-    ssl_certificate /etc/nginx/cert/${domain}_ecc/fullchain.cer;
-    ssl_certificate_key /etc/nginx/cert/${domain}_ecc/${domain}.key;
-    ssl_trusted_certificate /etc/nginx/cert/${domain}_ecc/ca.cer;
+    ssl_certificate /etc/nginx/ssl/${domain}/fullchain.cer;
+    ssl_certificate_key /etc/nginx/ssl/${domain}/${domain}.key;
+    ssl_trusted_certificate /etc/nginx/ssl/${domain}/ca.cer;
     ssl_session_timeout 5m;
     ssl_protocols TLSv1.2 TLSv1.3;
     ssl_ciphers 'TLS13+AESGCM+AES128:TLS13+AESGCM+AES256:TLS13+CHACHA20:TLS13+AES128+CCM:TLS13+AES256+CCM:TLS13+AES128+CCM8:TLS13+AES256+CCM8:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-CCM-SHA256:ECDHE-ECDSA-AES256-CCM-SHA384:ECDHE-ECDSA-AES128-CCM8-SHA256:ECDHE-ECDSA-AES256-CCM8-SHA256:ECDHE-RSA-AES128-CCM-SHA256:ECDHE-RSA-AES256-CCM-SHA384:ECDHE-RSA-AES128-CCM8-SHA256:ECDHE-RSA-AES256-CCM8-SHA256';
     ssl_session_cache shared:SSL:10m;
 
-    ssl_dhparam /etc/nginx/cert/dhparam.pem;
+    ssl_dhparam /etc/nginx/ssl/dhparam.pem;
+
+    location ~ /.well-known {
+        allow all;
+    }
 
     access_log /var/log/nginx/$domain.access.log;
 }" >> /etc/nginx/sites-available/$domain
     else
-        echo -e "${RED}证书申请失败${NC}"
-        rm /etc/nginx/sites-enabled/$domain
-        rm /etc/nginx/sites-available/$domain
-        rm -rf /var/www/$domain
-        rm -rf /etc/nginx/cert/${domain}_ecc
+        echo -e "${RED}安装证书失败${NC}"
+        echo -e "${RED}请手动安装证书${NC}"
+        echo -e "acme.sh --installcert -d $domain \
+            --fullchain-file /etc/nginx/ssl/${domain}/fullchain.cer \
+            --keypath /etc/nginx/ssl/${domain}/${domain}.key \
+            --ca-file /etc/nginx/ssl/${domain}/ca.cer \
+            --reloadcmd \"systemctl reload nginx\""
         exit 1
     fi
 
     if nginx -t; then
         nginx -s reload
     else
-        rm /etc/nginx/sites-enabled/$domain
-        rm /etc/nginx/sites-available/$domain
-        rm -rf /var/www/$domain
-        rm -rf /etc/nginx/cert/${domain}_ecc
         echo -e "${RED}nginx HTTPS 配置错误${NC}"
         exit 1
     fi
 }
 
 removesite() {
-    if [ -f /etc/nginx/sites-available/$2 ]; then
-        if [ -f /etc/nginx/sites-enabled/$2 ]; then
-            rm /etc/nginx/sites-enabled/$2
+    if [ -f /etc/nginx/sites-available/$domain ]; then
+        if [ -f /etc/nginx/sites-enabled/$domain ]; then
+            rm /etc/nginx/sites-enabled/$domain
         fi
-        rm /etc/nginx/sites-available/$2
+        rm /etc/nginx/sites-available/$domain
         nginx -s reload
     else
         echo -e "${RED}站点不存在${NC}"
@@ -122,8 +149,8 @@ listsiteall() {
 }
 
 enablesite() {
-    if [ -f /etc/nginx/sites-available/$2 ]; then
-        ln -s /etc/nginx/sites-available/$2 /etc/nginx/sites-enabled/$2
+    if [ -f /etc/nginx/sites-available/$domain ]; then
+        ln -s /etc/nginx/sites-available/$domain /etc/nginx/sites-enabled/$domain
         nginx -s reload
     else
         echo -e "${RED}站点不存在${NC}"
@@ -132,8 +159,8 @@ enablesite() {
 }
 
 disablesite() {
-    if [ -f /etc/nginx/sites-enabled/$2 ]; then
-        rm /etc/nginx/sites-enabled/$2
+    if [ -f /etc/nginx/sites-enabled/$domain ]; then
+        rm /etc/nginx/sites-enabled/$domain
         nginx -s reload
     else
         echo -e "${RED}站点不存在或未启用${NC}"
