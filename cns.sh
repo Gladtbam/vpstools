7162c08ef5926a5b3d70042352350eeffa556231
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2024-07-30 17:03:00 +0800

    Update cns.sh

--- cns.sh
+++ cns.sh
@@ -39,10 +39,6 @@ server {
     root /var/www/$domain;
     index index.html index.htm index.php;
 
-    location / {
-        return 301 https://\$host\$request_uri;
-    }
-
     location ~ /.well-known {
         allow all;
     }
@@ -85,6 +81,8 @@ AddSSL() {
     if [ $? -eq 0 ]; then
         echo " \
 server {
+    listen 80;
+    # listen [::]:80;
     listen 443 ssl http2;
     # listen [::]:443 ssl http2;
     server_name $domain;
@@ -94,19 +92,32 @@ server {
     ssl_certificate /etc/nginx/ssl/${domain}/fullchain.cer;
     ssl_certificate_key /etc/nginx/ssl/${domain}/${domain}.key;
     ssl_trusted_certificate /etc/nginx/ssl/${domain}/ca.cer;
-    ssl_session_timeout 5m;
+
     ssl_protocols TLSv1.2 TLSv1.3;
-    ssl_ciphers 'TLS13+AESGCM+AES128:TLS13+AESGCM+AES256:TLS13+CHACHA20:TLS13+AES128+CCM:TLS13+AES256+CCM:TLS13+AES128+CCM8:TLS13+AES256+CCM8:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-CCM-SHA256:ECDHE-ECDSA-AES256-CCM-SHA384:ECDHE-ECDSA-AES128-CCM8-SHA256:ECDHE-ECDSA-AES256-CCM8-SHA256:ECDHE-RSA-AES128-CCM-SHA256:ECDHE-RSA-AES256-CCM-SHA384:ECDHE-RSA-AES128-CCM8-SHA256:ECDHE-RSA-AES256-CCM8-SHA256';
+    ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305;
+    ssl_prefer_server_ciphers on;
+
+    ssl_session_timeout 1d;
     ssl_session_cache shared:SSL:10m;
+    ssl_session_tickets off;
+    
+    ssl_stapling on;
+    ssl_stapling_verify on;
+
+    add_header Strict-Transport-Security 'max-age=31536000; includeSubDomains; preload' always;
 
     ssl_dhparam /etc/nginx/ssl/dhparam.pem;
+
+    location / {
+        return 301 https://\$host\$request_uri;
+    }
 
     location ~ /.well-known {
         allow all;
     }
 
     access_log /var/log/nginx/$domain.access.log;
-}" >> /etc/nginx/sites-available/$domain
+}" > /etc/nginx/sites-available/$domain
     else
         echo -e "${RED}安装证书失败${NC}"
         echo -e "${RED}请手动安装证书${NC}"
