812e3475aff38476c94e6d06e68fccada75e1333
Gladtbam <49943258+Gladtbam@users.noreply.github.com>
2025-01-05 17:53:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -40,38 +40,38 @@ Install_ACME() {
     read -p "请输入邮箱地址：" email
     if [ ! -f /etc/nginx/ssl ]; then
         mkdir -p /etc/nginx/ssl && cd $_
-        openssl dhparam -out dhparam.pem 4096
+        time openssl dhparam -rand /dev/random -out dhparam.pem 4096
     fi
 
     if [ ! -f /usr/local/bin/acme.sh ]; then
         cd /tmp
         git clone --depth 1 https://github.com/acmesh-official/acme.sh.git
-        ./acme.sh/acme.sh --install --home /usr/local/bin/acme.sh --accountemail $email --cert-home /etc/nginx/ssl
+        ./acme.sh/acme.sh --install --home /usr/local/acme.sh --accountemail $email
         source ~/.bashrc
         rm -rf /tmp/acme.sh
         acme.sh --upgrade --auto-upgrade
         acme.sh --set-default-ca --server letsencrypt
     fi
 
-    if [ ! -f /var/www/html/.well-known/acme-challenge ]; then
-        mkdir -p /var/www/html/.well-known/acme-challenge
-        chown -R www-data:www-data /var/www/html/.well-known/acme-challenge
-        chmod -R 555 /var/www/html/.well-known/acme-challenge
-    fi
+    # if [ ! -f /var/www/html/.well-known/acme-challenge ]; then
+    #     mkdir -p /var/www/html/.well-known/acme-challenge
+    #     chown -R www-data:www-data /var/www/html/.well-known/acme-challenge
+    #     chmod -R 555 /var/www/html/.well-known/acme-challenge
+    # fi
 
-    cat << EOF > /etc/nginx/conf.d/letsencrypt-webroot.conf > /dev/null
-server {
-    listen 80;
-    server_name _;
-    location /.well-known/acme-challenge/ {
-        alias /var/www/html/.well-known/acme-challenge/;
-    }
-}
-EOF
-    if nginx -t; then
-        nginx -s reload
-    fi
-}
+#     cat << EOF > /etc/nginx/conf.d/letsencrypt-webroot.conf > /dev/null
+# server {
+#     listen 80;
+#     server_name _;
+#     location /.well-known/acme-challenge/ {
+#         alias /var/www/html/.well-known/acme-challenge/;
+#     }
+# }
+# EOF
+#     if nginx -t; then
+#         nginx -s reload
+#     fi
+# }
 
 Install_MySQL() {
     echo "${YELLOW} 下载地址：https://dev.mysql.com/downloads/ ${NC}"
